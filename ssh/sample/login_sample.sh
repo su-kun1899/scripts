7325add438580175844af7ee9bebe5b3754ddd7d
su-kun1899 <higedrum.coz@gmail.com>
2016-11-27 20:10:02 +0900

    Refactor

--- ssh/sample/login_sample.sh
+++ ssh/sample/login_sample.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 cwd=`dirname "${0}"`
-. $cwd/../auto_login.sh
+. ${cwd}/../auto_login.sh
 
 # settings
 host='your host name'
 login_name='your account'
 password='your password'
 
-auto_login $host $login_name $password
+auto_login ${host} ${login_name} ${password}
